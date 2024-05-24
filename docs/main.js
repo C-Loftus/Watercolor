0dfdfd7c89fcd3ab99ca54013d3c64b4f7e3dc1b
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-05-24 11:59:00 -0400

    better docs

--- docs/main.js
+++ docs/main.js
@@ -1,13 +1,30 @@
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function () {
-//   myFunction();
-// };
+document.addEventListener("DOMContentLoaded", () => {
+  const copyIcons = document.querySelectorAll(".copy");
 
-// function myFunction() {
-//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-//   var height =
-//     document.documentElement.scrollHeight -
-//     document.documentElement.clientHeight;
-//   var scrolled = (winScroll / height) * 100;
-//   document.getElementById("myBar").style.width = scrolled + "%";
-// }
+  copyIcons.forEach((icon) => {
+    icon.addEventListener("click", (event) => {
+      const textToCopy = icon.parentElement.innerText;
+      navigator.clipboard
+        .writeText(textToCopy)
+        .then(() => {
+          const tooltip = document.createElement("div");
+          tooltip.classList.add("tooltip");
+          tooltip.textContent = "Copied to clipboard";
+
+          // Calculate tooltip position
+          const iconRect = icon.getBoundingClientRect();
+          tooltip.style.top = iconRect.top + "px";
+          tooltip.style.left = iconRect.right + "px";
+
+          document.body.appendChild(tooltip);
+
+          setTimeout(() => {
+            tooltip.remove();
+          }, 1000); // Remove tooltip after 2 seconds
+        })
+        .catch((err) => {
+          console.error("Could not copy text: ", err);
+        });
+    });
+  });
+});
